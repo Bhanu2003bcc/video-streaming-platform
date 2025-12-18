00c60e371bf3fc88c96319d0cf46e1e7916fb71c
Mr. Er <bhanupratapsingh2939522@gmail.com>
2025-12-18 23:44:44 +0530

    Config service for centralised configuration

--- api-gateway/src/main/java/com/mrer/api_gateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/mrer/api_gateway/ApiGatewayApplication.java
@@ -2,9 +2,7 @@ package com.mrer.api_gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableDiscoveryClient
