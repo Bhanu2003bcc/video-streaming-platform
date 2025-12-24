88a542956719cb64f5bb5499425928cce9a99e96
Mr. Er <bhanupratapsingh2939522@gmail.com>
2025-12-24 16:33:41 +0530

    Auth service provide the service for user registration and login with jwt token generation.

--- Auth-service/src/main/java/com/mrer/Auth_service/contoller/AuthController.java
+++ Auth-service/src/main/java/com/mrer/Auth_service/contoller/AuthController.java
@@ -1,6 +1,7 @@
 package com.mrer.Auth_service.contoller;
 
 import com.mrer.Auth_service.model.User;
+import com.mrer.Auth_service.repository.UserRepository;
 import com.mrer.Auth_service.service.AuthService;
 import com.mrer.Auth_service.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Map;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/auth")
@@ -19,6 +21,10 @@ public class AuthController {
 
     @Autowired
     private UserService userService;
+
+    @Autowired
+    private UserRepository repository;
+
 
     @PostMapping("/register")
     public ResponseEntity<?> register(@RequestBody Map<String, String> req) {
@@ -39,4 +45,20 @@ public class AuthController {
         List<User> users = userService.getAllUsers();
         return ResponseEntity.ok(users);
     }
+
+    @GetMapping("/me")
+    public ResponseEntity<?> me(@RequestHeader("X-USER-ID") String userId) {
+        User user = repository.findById(UUID.fromString(userId))
+                .orElseThrow(() -> new RuntimeException("User not found"));
+
+        return ResponseEntity.ok(
+                Map.of(
+                        "id", user.getId(),
+                        "email", user.getEmail(),
+                        "role", user.getRole(),
+                        "createdAt", user.getCreatedAt()
+                )
+        );
+    }
+
 }
